8874f90e7147ca4c34d9818dc330061cefde7679
AK51886 <anton.kirzyk@citi.com>
2019-06-04 16:24:48 +0300

    main

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
-from flask import Flask
+from flask import Flask, request
+import telebot
 
-app = Flask(__name__)
+server = Flask(__name__)
 
-@app.route('/'):
-def index():
-    return 'boroda'
+bot = telebot.TeleBot("864277689:AAF12Kz_E-rUrEAy35i6gVKZC1c-nJ-tCUw")
+keyboard = Keyboard(bot)
 
-if __name__ == "__main__":
-    app.run()
+@bot.message_handler(commands=['start'])
+def handle_text(message):
+    user_markup = telebot.types.ReplyKeyboardMarkup(True, False)
+    user_markup.row('Получить расписание')
+    user_markup.row('Получить расписание по подписке')
+    user_markup.row('Время пар')
+    user_markup.row('Обновления', 'Обратная связь')
+    bot.send_message(message.from_user.id, 'Выберите пункт меню:', reply_markup=user_markup)
+
+    @server.route('/' + token, methods=['POST'])
+def get_message():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "POST", 200
+
+
+@server.route("/")
+def web_hook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://botteleapp.herokuapp.com/' + "864277689:AAF12Kz_E-rUrEAy35i6gVKZC1c-nJ-tCUw")
+    return "CONNECTED", 200
+
+server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
